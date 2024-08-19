799e1a63ecbb109646d6f0e2f4d69f8a4539c83b
ify <ify@apishore.comgit config --local user.name ifyapishore>
2024-08-19 10:02:43 +0500

    test

---
+++ git-connect.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git remote set-url origin git@github.com-apishore:apishore/experiment-qa.git
+git config --local user.email "ify@apishore.com""
+git config --local user.name "ifyapishore
+" 
